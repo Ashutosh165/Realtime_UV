2a44ebbea9e88253934cc3bc503df78272bed8f8
Ashutosh165 <167576781+Ashutosh165@users.noreply.github.com>
2025-07-30 01:34:52 +0530

    Update main.jsx

--- Frontend/realtimeuv/src/main.jsx
+++ Frontend/realtimeuv/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './pages/Auth/Login.jsx'
-import SignUp from './pages/Auth/SignUp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
